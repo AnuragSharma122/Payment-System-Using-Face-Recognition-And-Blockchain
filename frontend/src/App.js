b480c79cf4a4fff30cd0f68730fc6273af9f56e3
AnuragSharma122 <82510045+AnuragSharma122@users.noreply.github.com>
2023-04-24 19:45:16 +0530

    Added metamask wallet authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-// import './App.css';
-
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NotFound } from "./components/NotFound";
 import { RegisterPage } from "./pages/RegisterPage";
@@ -10,17 +9,57 @@ import "./assets/scss/blk-design-system-react.scss";
 import "./assets/demo/demo.css";
 // import {Home} from './pages/Home';
 function App() {
+  const [isMetamaskConnected, setIsMetamaskConnected] = useState(false);
+
+  useEffect(() => {
+    async function checkMetamaskConnection() {
+      if (typeof window.ethereum !== "undefined") {
+        // Metamask is installed
+        if (window.ethereum.isConnected()) {
+          // Metamask is connected
+          setIsMetamaskConnected(true);
+        } else {
+          // Metamask is not connected
+          setIsMetamaskConnected(false);
+        }
+      } else {
+        // Metamask not installed
+        setIsMetamaskConnected(false);
+      }
+    }
+
+    checkMetamaskConnection();
+  }, []);
+
+  const connectToMetamask = async () => {
+    try {
+      // Request access to the user's Metamask wallet
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      setIsMetamaskConnected(true);
+    } catch (error) {
+      console.log(error);
+      setIsMetamaskConnected(false);
+    }
+  };
+
   return (
     <div className="App">
-      {/* <ExamplesNavbar /> */}
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route exact path="/payment" element={<PaymentsPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      {isMetamaskConnected ? (
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route exact path="/payment" element={<PaymentsPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      ) : (
+        <div>
+          <h1>Connect to Metamask Wallet</h1>
+          <p>Please connect to your Metamask wallet to use this application.</p>
+          <button onClick={connectToMetamask}>Connect to Metamask</button>
+        </div>
+      )}
     </div>
   );
 }
